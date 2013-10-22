4824701391a730ca95090c17dc0d16d5d5485b1d
Michał Bachowski <michal@bachowski.pl>
2013-10-22 21:30:20 +0200

    Added tests for pycomber.value_objects.ImmutableDict

--- test/value_objects_test.py
+++ test/value_objects_test.py
@@ -5,25 +5,33 @@
 # python standard library
 #
 import unittest
+from functools import partial
 
 ##
 # content bits modules
 #
-from pycomber.merger import Merger
+from pycomber.value_objects import ImmutableDict
 
 
-class MergerPointTestCase(unittest.TestCase):
+class ImmutableDictTestCase(unittest.TestCase):
 
-    def setUp(self):
-        self.merger = Merger()
 
-    def test_init_requires_no_arguments(self):
-        err = False
-        try:
-            Merger()
-        except AttributeError:
-            err = True
-        self.assertFalse(err)
+    def test_init_requires_one_argument(self):
+        self.assertRaises(TypeError, ImmutableDict)
+
+    def test_instance_is_immutable(self):
+        obj = ImmutableDict({'a': 1})
+        self.assertRaises(TypeError, partial(obj.__setitem__, 'a', 2))
+        self.assertRaises(TypeError, partial(obj.__setitem__, 'b', 1))
+        self.assertRaises(TypeError, partial(obj.__delitem__, 'a'))
+        self.assertRaises(TypeError, partial(obj.update, {'b': 1}))
+
+    def test_instance_behaves_as_dict(self):
+        obj = ImmutableDict({'a': 1})
+        self.assertEqual(obj['a'], 1)
+        self.assertEqual(len(obj), 1)
+        self.assertTrue('a' in obj)
+        self.assertEqual(list(iter(obj)), ['a'])
 
 
 if "__main__" == __name__:
