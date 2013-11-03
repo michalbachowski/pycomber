0ea2f7b85ee0ba88bc631d7e5821d22f41af8ee1
Michał Bachowski <michal@bachowski.pl>
2013-11-03 13:22:45 +0100

    Added tests for MergeAbstract, MergeNone and MergePrimitives

--- test/strategies_test.py
+++ test/strategies_test.py
@@ -5,25 +5,91 @@
 # python standard library
 #
 import unittest
+from functools import partial
+
+
+##
+# test helper
+#
+from testutils import mock
+
 
 ##
 # content bits modules
 #
-from pycomber.merger import Merger
+from pycomber.strategies import MergeAbstract, MergeList, MergeListOverride, \
+    MergeSet, MergeSetOverride, MergeDict, MergeDictOverride, MergePrimitives, \
+    MergeNone
 
 
-class MergerPointTestCase(unittest.TestCase):
+class MergeTestMixin(object):
 
     def setUp(self):
-        self.merger = Merger()
+        self.manager = mock.Mock()
+        self.merger = self.merger_class(self.manager)
 
-    def test_init_requires_no_arguments(self):
+    def test_init_requires_one_argument(self):
+        self.assertRaises(TypeError, self.merger_class)
         err = False
         try:
-            Merger()
+            self.merger_class(None)
         except AttributeError:
             err = True
         self.assertFalse(err)
+
+    def test_instance_is_callable(self):
+        self.assertTrue(hasattr(self.merger, '__call__'))
+
+    def test_call_expects_2_arguments(self):
+        self.assertRaises(TypeError, self.merger)
+        self.assertRaises(TypeError, partial(self.merger, None))
+        err=False
+        try:
+            self.merger(None, None)
+        except TypeError:
+            err=True
+        except:
+            pass
+        self.assertFalse(err)
+        self.assertRaises(TypeError, partial(self.merger, None, None, None))
+
+
+class MergeAbstractTestCase(unittest.TestCase, MergeTestMixin):
+
+    def setUp(self):
+        self.merger_class = MergeAbstract
+        MergeTestMixin.setUp(self)
+
+    def test_call_must_be_implemented(self):
+        self.assertRaises(RuntimeError, partial(self.merger, None, None))
+
+
+class MergeNoneTestCase(unittest.TestCase, MergeTestMixin):
+
+    def setUp(self):
+        self.merger_class = MergeNone
+        MergeTestMixin.setUp(self)
+
+    def test_non_None_arguments_is_returned(self):
+        self.assertEqual(self.merger(None, 'b'), 'b')
+        self.assertEqual(self.merger('a', None), 'a')
+        self.assertEqual(self.merger('a', 'b'), 'a')
+        self.assertEqual(self.merger(None, None), None)
+
+
+class MergePrimitivesTestCase(unittest.TestCase, MergeTestMixin):
+
+    def setUp(self):
+        self.merger_class = MergePrimitives
+        MergeTestMixin.setUp(self)
+
+    def test_merge_from_is_alwasy_returned(self):
+        self.assertEqual(self.merger(None, 'b'), None)
+        self.assertEqual(self.merger('a', None), 'a')
+        self.assertEqual(self.merger('a', 'b'), 'a')
+        self.assertEqual(self.merger(None, None), None)
+
+
 
 
 if "__main__" == __name__:
