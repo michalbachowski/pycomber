626c346897784fa78c7eae20f18ef95ef19a45f2
Michał Bachowski <michal@bachowski.pl>
2013-11-05 09:27:19 +0100

    Added tests for pycomber.manager.Manager: set_strategy() and get_strategy()

--- test/manager_test.py
+++ test/manager_test.py
@@ -59,6 +59,51 @@ class ManagerTestCase(unittest.TestCase):
         self.manager.set_factory(int, 'a')
         self.assertEqual(self.manager.get_factory(int), 'a')
 
+    def test_set_strategy_expects_2_arguments(self):
+        self.assertRaises(TypeError, self.manager.set_strategy)
+        self.assertRaises(TypeError, partial(self.manager.set_strategy, None))
+        self.assertRaises(TypeError, partial(self.manager.set_strategy, None, \
+                                                            None, None, None))
+        err = False
+        try:
+            self.manager.set_strategy(None, None)
+        except TypeError, e:
+            err = 'expected at least' in e
+        self.assertFalse(err)
+
+    def test_set_strategy_allows_3_arguments(self):
+        err = False
+        try:
+            self.manager.set_strategy(None, None, None)
+        except TypeError, e:
+            err = 'expected at least' in e
+        self.assertFalse(err)
+
+    def test_set_strategy_for_single_types_makes_one_pair(self):
+        self.manager.set_strategy('strategy', int, int)
+        self.assertEqual(self.manager.get_strategy(int, int), 'strategy')
+
+    def test_set_strategy_for_set_of_types_makes_cartesian(self):
+        self.manager.set_strategy('strategy', (int, str), (int, str))
+        for a, b in ((int, int), (int, str), (str, str), (str, int)):
+            self.assertEqual(self.manager.get_strategy(a, b), 'strategy')
+
+    def test_set_strategy_for_mixed_types_makes_pairs(self):
+        self.manager.set_strategy('strategy', int, (int, str))
+        for a, b in ((int, int), (int, str)):
+            self.assertEqual(self.manager.get_strategy(a, b), 'strategy')
+        self.assertRaises(TypeError, partial(self.manager.get_strategy, str, \
+                                                                        str))
+
+    def test_set_strategy_treats_string_as_set(self):
+        self.manager.set_strategy('strategy', 'ab', 'cd')
+        for a, b in (('a', 'c'), ('a', 'd'), ('b', 'c'), ('b', 'd')):
+            self.assertEqual(self.manager.get_strategy(a, b), 'strategy')
+
+    def test_get_stragety_returns_predefined_strategy(self):
+        self.manager.set_strategy('strategy', 'ab', 'cd')
+        for a, b in (('a', 'c'), ('a', 'd'), ('b', 'c'), ('b', 'd')):
+            self.assertEqual(self.manager.get_strategy(a, b), 'strategy')
 
 
 if "__main__" == __name__:
